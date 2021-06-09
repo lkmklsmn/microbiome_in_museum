db459e027203b413ef45675621604296751a8f38
lkmklsmn <lkmklsmn@gmail.com>
2021-06-09 18:38:15 -0500

    Update run_qiime2_commands.sh

--- run_qiime2_commands.sh
+++ run_qiime2_commands.sh
@@ -2,6 +2,6 @@ cd
 
 qiime tools import \
 --type 'SampleData[PairedEndSequencesWithQuality]' \
---input-path qiime/manifest.txt \
---output-path qiime2/lounsbery_seq2.qza \
---input-format PairedEndFastqManifestPhred64
+--input-path /storage/thinc/data/projects/lounsbery/qiime/manifest.txt \
+--output-path /storage/thinc/data/projects/lounsbery/qiime/lounsbery_seq2.qza \
+--input-format PairedEndFastqManifestPhred33V2
