cbfb68008a177596c2773ae9ac8917c112d35dbf
lkmklsmn <lkmklsmn@gmail.com>
2021-06-09 18:37:07 -0500

    Update create_qiime2_manifest_file.R

--- create_qiime2_manifest_file.R
+++ create_qiime2_manifest_file.R
@@ -4,7 +4,7 @@ tmp <- tmp[grep("fastq.gz", tmp)]
 dir <- getwd()
 
 files <- unlist(lapply(tmp, function(x) paste(dir, x, sep = "/")))
-sample <- gsub("Lounsbery@DSMZ/Seq_Data/seq2//", "", fixed = T, tmp)
+sample <- gsub("Lounsbery@DSMZ/Seq_Data/seq2/", "", fixed = T, tmp)
 sample <- sample[seq(1, length(sample), by = 2)]
 sample <- gsub("_R1.fastq.gz", "", fixed = T, sample)
 
@@ -13,3 +13,5 @@ read_2 <- files[seq(2, length(files), by = 2)]
 
 manifest <- data.frame(sample_id = sample,forward_absolute_filepath = read_1, reverse_absolute_filepath = read_2)
 write.table(manifest, row.names = F, "qiime/manifest.txt", sep = "\t", quote = F)
+                       
+# Fix column names by switching sample_id to sample-id using vim or other editor
