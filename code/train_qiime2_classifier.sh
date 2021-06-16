70931ffcbbcb13a23e2e738ee8672f401e9a24d9
lkmklsmn <lkmklsmn@gmail.com>
2021-06-16 10:00:06 -0500

    specify V3-4 region

--- code/train_qiime2_classifier.sh
+++ code/train_qiime2_classifier.sh
@@ -12,6 +12,18 @@ qiime tools import \
 --input-path SILVA_132_QIIME_release/taxonomy/16S_only/99/majority_taxonomy_7_levels.txt \
 --output-path 99_otus_16S_taxonomy.qza
 
+qiime feature-classifier extract-reads \
+--i-sequences 99_otus_16S.qza \
+# 341f
+--p-f-primer CCTACGGGWGGCWGCAG \
+# 803r 
+--p-r-primer CTACCRGGGTATCTAATCC \
+--p-min-length 285 \
+--p-max-length 600 \
+--o-reads ref_seqs_16S_V3-V4.qza \
+--verbose \
+&> 16S_V3-V4_training.log
+
 qiime feature-classifier fit-classifier-naive-bayes \
 --i-reference-reads 99_otus_16S.qza \
 --i-reference-taxonomy 99_otus_16S_taxonomy.qza \
