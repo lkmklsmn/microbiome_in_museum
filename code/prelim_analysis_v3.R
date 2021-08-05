e6ec8e3f819308af65589c5334c4f45eb50826c7
lkmklsmn <lkmklsmn@gmail.com>
2021-08-05 14:51:41 -0500

    Update prelim_analysis_v3.R

--- code/prelim_analysis_v3.R
+++ code/prelim_analysis_v3.R
@@ -2,6 +2,7 @@
 library(qiime2R)
 library(phyloseq)
 library(readxl)
+library(ggplot2)
 
 setwd("/Users/lukas/OneDrive/Documents/GitHub/lounsbery/")
 
@@ -86,11 +87,11 @@ p <- phyloseq_miko(OTU, TAX, info)
 ok = genefilter_sample(p, filterfun_sample(function(x) x > 5), A=0.05*nsamples(p))
 GP1 = prune_taxa(ok, p)
 
-ok = is.na(TAX[,"Phylum"])
-GP1 = prune_taxa(ok, GP1)
+ok = is.na(as.character(TAX[,"Phylum"]))
+GP1 = prune_taxa(ok, p)
 
 ok = names(which(colSums(OTU) > 100))
-GP1 = prune_samples(ok, GP1)
+GP1 = prune_samples(ok, p)
 
 GP1 = transform_sample_counts(GP1, function(x) 1E6 * x/sum(x))
 
@@ -103,15 +104,15 @@ aframe <- data.frame(GP.ord$vectors, sample_data(GP1))
 aframe$label <- NA
 aframe$label[aframe$type.of.room == ""] <- aframe$sample.id[aframe$type.of.room == ""]
 
-ggplot(aframe, aes(Axis.1, Axis.2, shape = Institution, color = Object, label = label)) +
+ggplot(aframe, aes(Axis.1, Axis.2, shape = institution, color = object, label = label)) +
   geom_point() + ggrepel::geom_label_repel() +
   theme_bw()
 
-ggplot(aframe, aes(Axis.1, Axis.2, shape = Institution, color = spot == "Air")) +
+ggplot(aframe, aes(Axis.1, Axis.2, shape = institution, color = spot == "Air")) +
   geom_point() +
   theme_bw()
 
-ggplot(aframe, aes(Axis.3, Axis.4, shape = Institution, color = factor(Object.number))) +
+ggplot(aframe, aes(Axis.3, Axis.4, shape = institution, color = factor(object_number))) +
   geom_point() +
   theme_bw()
 
@@ -136,24 +137,24 @@ aframe <- data.frame(meta, total = colSums(counts), pca$x[, 1:10])
 mds <- cmdscale(dist(t(normalized)), k = 2)
 aframe <- data.frame(meta, total = colSums(counts), pca$x[, 1:10], mds)
 
-ggplot(aframe, aes(X1, X2, color = Institution, shape = Institution, label = sample.id)) +
+ggplot(aframe, aes(X1, X2, color = institution, shape = institution, label = sample.id)) +
   geom_point() + geom_label() +
   theme_classic()
 
-ggplot(aframe, aes(X1, X2, color = log10(total), shape = Institution)) +
+ggplot(aframe, aes(X1, X2, color = log10(total), shape = institution)) +
   geom_point() +
   theme_classic()
 
-ggplot(aframe, aes(PC1, PC2, color = log10(total), shape = Institution)) +
+ggplot(aframe, aes(PC1, PC2, color = log10(total), shape = institution)) +
   geom_point() +
   theme_classic()
 
-ggplot(aframe, aes(PC1, PC2, color = Institution, shape = Institution, label = sample.id)) +
+ggplot(aframe, aes(PC1, PC2, color = institution, shape = institution, label = sample.id)) +
   geom_point() + geom_label() +
   theme_classic()
 
 # Run positive unlabeled learning ####
-outcome <- meta$Institution == "Control"
+outcome <- meta$institution == "DSMZ"
 positives <- which(outcome)
 unlabeled <- which(!outcome)
 predictions <- do.call(rbind, lapply(1:100, function(x){
@@ -171,13 +172,13 @@ avg_pred <- apply(predictions, 2, function(x) mean(x, na.rm = T))
 meta$avg_pred <- avg_pred
 aframe <- data.frame(avg_pred, total = colSums(counts), meta)
 
-p1 <- ggplot(aframe, aes(avg_pred, log10(total), color = Institution == "Control", shape = Institution)) +
+p1 <- ggplot(aframe, aes(avg_pred, log10(total), color = collection == "Control", shape = institution)) +
   geom_point() +
   ylab("Total count [log10]") + xlab("Average prediction") +
   ggtitle("Positive-unlabeled learning") +
   theme_bw()
 
-p2 <- ggplot(aframe, aes(avg_pred, log10(total), color = spot == "Air", shape = Institution)) +
+p2 <- ggplot(aframe, aes(avg_pred, log10(total), color = spot == "Air", shape = institution)) +
   geom_point() +
   ylab("Total count [log10]") + xlab("Average prediction") +
   ggtitle("Positive-unlabeled learning") +
@@ -188,7 +189,7 @@ gridExtra::grid.arrange(p1, p2, ncol = 2)
 # Run analysis on good lion samples ####
 ok <- which(aframe$avg_pred < 1.5)
 tmp <- meta[ok,]
-ok_samples <- tmp$sample.id[tmp$Object %in% c("Lion left", "Lion right")]
+ok_samples <- tmp$sample.id[grep("Lion", tmp$object)]
 counts_tmp <- counts[, ok_samples]
 zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
 counts_tmp <- counts_tmp[which(zeros > 1),]
@@ -199,13 +200,13 @@ normalized <- sqrt(normalized)
 mds <- cmdscale(dist(t(normalized)), k = 2)
 aframe_tmp <- data.frame(meta[match(ok_samples, meta$sample.id), ], total = colSums(counts_tmp), mds)
 
-ggplot(aframe_tmp, aes(X1, X2, color = touched, shape = Object)) +
+ggplot(aframe_tmp, aes(X1, X2, color = comments, shape = object)) +
   geom_point() +
   ggtitle("MDS - Lion") +
   theme_classic()
 
-# Hellenistic Haal ####
-ok_samples <- tmp$sample.id[tmp$Object %in% c("Hellenistic Haal")]
+# Hellenistic Hall ####
+ok_samples <- tmp$sample.id[grep("Hellenistic", tmp$object)]
 counts_tmp <- counts[, ok_samples]
 zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
 counts_tmp <- counts_tmp[which(zeros > 1),]
@@ -222,7 +223,7 @@ ggplot(aframe_tmp, aes(X1, X2, label = spot)) +
   theme_classic()
 
 # Tendaguru ####
-tmp <- meta[which(meta$Object == "Tendaguru"), ]
+tmp <- meta[which(meta$object == "Tendaguru"), ]
 ok_samples <- tmp$sample.id
 counts_tmp <- counts[, ok_samples]
 zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
@@ -237,7 +238,8 @@ aframe_tmp <- data.frame(meta[match(ok_samples, meta$sample.id), ], total = colS
 
 ggplot(aframe_tmp, aes(X1, X2, color = comments)) +
   geom_point() +
-  ggtitle("MDS - Tendaguru") +
+  ggtitle("MDS - Tendaguru") + 
+  xlab("MDS1") + ylab("MDS2") +
   theme_classic()
 
 ggplot(aframe_tmp, aes(PC1, PC2, color = comments)) +
@@ -261,57 +263,249 @@ ok_features <- intersect(rownames(normalized), names(which(TAX@.Data[, "Genus"] 
 boxplot(split(colMeans(normalized[ok_features,]), tmp$comments),
         ylab = "Streptococcus abundance")
 
+# Mollucs ####
+tmp <- meta[which(meta$collection == "Mollucs"), ]
+ok_samples <- tmp$sample.id
+counts_tmp <- counts[, ok_samples]
+zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
+counts_tmp <- counts_tmp[which(zeros > 1),]
 
-# Run diff expr
-library(DESeq2)
+counts_tmp <- counts_tmp[, colSums(counts_tmp) > 100]
 
-des <- DESeqDataSetFromMatrix(countData = Down_Sample_Matrix(counts_tmp), colData = tmp, design = ~ comments)
-sfs <- log10(colSums(counts[, colnames(counts_tmp)])) / median(log10(colSums(counts[, colnames(counts_tmp)])))
-sizeFactors(des) <- sfs
-sizeFactors(des) <- rep(1, ncol(des))
-des <- DESeq(des)
-res <- results(des, contrast = c("comments", "touched by users", "untouched"))
-res <- data.frame(res, TAX@.Data[rownames(res),])
-res <- res[sort.list(res$pvalue),]
+normalized <- t(t(counts_tmp)/colSums(counts_tmp))
+normalized <- sqrt(normalized)
 
-ggplot(res, aes(log2FoldChange, -log10(pvalue), color = Order)) +
+mds <- cmdscale(dist(t(normalized)), k = 2)
+pca <- prcomp(t(normalized), scale. = T)
+aframe_tmp <- data.frame(meta[match(colnames(counts_tmp), meta$sample.id), ], total = colSums(counts_tmp), mds, pca$x)
+
+ggplot(aframe_tmp, aes(X1, X2, color = comments, shape = object)) +
   geom_point() +
+  ggtitle("MDS - Mollucs") +
+  theme_classic()
+
+ggplot(aframe_tmp, aes(PC1, PC2, color = comments, shape = object)) +
+  geom_point() +
+  ggtitle("PCA - Mollucs") +
+  theme_classic()
+
+aframe_tmp <- data.frame(PC1 = pca$rotation[,1], TAX@.Data[rownames(pca$rotation), ])
+asplit <- split(aframe_tmp$PC1, aframe_tmp$Genus)
+aframe_tmp$Genus <- factor(aframe_tmp$Genus, levels = names(sort(unlist(lapply(asplit, median)))))
+ggplot(aframe_tmp[!is.na(aframe_tmp$Genus),], aes(PC1, Genus, color = Phylum)) +
+  geom_boxplot() +
+  xlab("PC1 loading") +
   theme_bw()
+
+ok_features <- intersect(rownames(normalized), names(which(TAX@.Data[, "Genus"] == "g__Streptococcus")))
+boxplot(split(colMeans(normalized[ok_features,]), tmp$comments),
+        ylab = "Streptococcus abundance")
+
+ok_features <- intersect(rownames(normalized), names(which(TAX@.Data[, "Genus"] == "g__Staphylococcus")))
+boxplot(split(colMeans(normalized[ok_features,]), tmp$comments),
+        ylab = "Staphylococcus abundance")
+
+
+# Combine into one analysis #
+tmp <- meta[meta$institution == "MfN",]
+ok_samples <- tmp$sample.id
+counts_tmp <- counts[, ok_samples]
+zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
+counts_tmp <- counts_tmp[which(zeros > 1),]
+
+counts_tmp <- counts_tmp[, colSums(counts_tmp) > 100]
+
+normalized <- t(t(counts_tmp)/colSums(counts_tmp))
+normalized <- sqrt(normalized)
+
+mds <- cmdscale(dist(t(normalized)), k = 2)
+pca <- prcomp(t(normalized), scale. = T)
+aframe_tmp <- data.frame(meta[match(colnames(counts_tmp), meta$sample.id), ], total = colSums(counts_tmp), mds, pca$x)
+
+aframe_tmp$touched <- "no"
+aframe_tmp$touched[grep("touch", aframe_tmp$comments)] <- "yes"
+ggplot(aframe_tmp, aes(X1, X2, color = touched, shape = object)) +
+  geom_point() +
+  ggtitle("MDS - Mollucs") +
+  xlab("MDS1") + ylab("MDS2") +
+  theme_classic()
+
+# SPK ####
+tmp <- meta[meta$institution == "SPK",]
+ok_samples <- tmp$sample.id
+counts_tmp <- counts[, ok_samples]
+zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
+counts_tmp <- counts_tmp[which(zeros > 1),]
+
+counts_tmp <- counts_tmp[, colSums(counts_tmp) > 100]
+
+normalized <- t(t(counts_tmp)/colSums(counts_tmp))
+normalized <- sqrt(normalized)
+
+mds <- cmdscale(dist(t(normalized)), k = 2)
+pca <- prcomp(t(normalized), scale. = T)
+aframe_tmp <- data.frame(meta[match(colnames(counts_tmp), meta$sample.id), ], total = colSums(counts_tmp), mds, pca$x)
+
+ggplot(aframe_tmp, aes(X1, X2, color = type_of_room, shape = type_of_room)) +
+  geom_point() +
+  ggtitle("MDS - SPK") +
+  xlab("MDS1") + ylab("MDS2") +
+  theme_classic()
+
+ggplot(aframe_tmp, aes(PC1, PC2, color = type_of_room, shape = material)) +
+  geom_point() +
+  ggtitle("PCA - SPK") +
+  theme_classic()
+
+# SPK Lions ####
+tmp <- meta[grep("Lion", meta$object),]
+ok_samples <- tmp$sample.id
+counts_tmp <- counts[, ok_samples]
+zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
+counts_tmp <- counts_tmp[which(zeros > 1),]
+
+counts_tmp <- counts_tmp[, colSums(counts_tmp) > 100]
+
+normalized <- t(t(counts_tmp)/colSums(counts_tmp))
+normalized <- sqrt(normalized)
+
+mds <- cmdscale(dist(t(normalized)), k = 2)
+pca <- prcomp(t(normalized), scale. = T)
+aframe_tmp <- data.frame(meta[match(colnames(counts_tmp), meta$sample.id), ], total = colSums(counts_tmp), mds, pca$x)
+
+ggplot(aframe_tmp, aes(X1, X2, color = comments, shape = object)) +
+  geom_point() +
+  ggtitle("MDS - Gate Lion") +
+  xlab("MDS1") + ylab("MDS2") +
+  theme_classic()
+
+ok_features <- intersect(rownames(normalized), names(which(TAX@.Data[, "Genus"] == "g__Streptococcus")))
+boxplot(split(colMeans(normalized[ok_features,]), tmp$comments),
+        ylab = "Streptococcus abundance")
+
+ok_features <- intersect(rownames(normalized), names(which(TAX@.Data[, "Genus"] == "g__Staphylococcus")))
+boxplot(split(colMeans(normalized[ok_features,]), tmp$comments),
+        ylab = "Staphylococcus abundance")
+
+
+# ####
+tmp <- meta[meta$type_of_room == "exhibition",]
+tmp <- tmp[tmp$object != "",]
+tmp <- tmp[tmp$object %in% c("Ishtar Gate, right side", "Procession Street, left side") ,]
+ok_samples <- tmp$sample.id
+counts_tmp <- counts[, ok_samples]
+zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
+counts_tmp <- counts_tmp[which(zeros > 1),]
+
+counts_tmp <- counts_tmp[, colSums(counts_tmp) > 100]
+
+normalized <- t(t(counts_tmp)/colSums(counts_tmp))
+normalized <- sqrt(normalized)
+
+mds <- cmdscale(dist(t(normalized)), k = 2)
+pca <- prcomp(t(normalized), scale. = T)
+aframe_tmp <- data.frame(meta[match(colnames(counts_tmp), meta$sample.id), ], total = colSums(counts_tmp), mds, pca$x)
+
+ggplot(aframe_tmp, aes(X1, X2, color = object, shape = object)) +
+  geom_point() +
+  ggtitle("MDS - SPK") +
+  theme_classic()
+
+ggplot(aframe_tmp, aes(X1, X2, color = avg_pred, shape = material)) +
+  geom_point() +
+  ggtitle("MDS - SPK") +
+  theme_classic()
+
+
+ggplot(aframe_tmp, aes(PC1, PC2, color = object, shape = object)) +
+  geom_point() +
+  ggtitle("PCA - SPK") +
+  theme_classic()
+
+aframe_tmp$condition <- "yes"
+aframe_tmp$condition[grep("white", aframe_tmp$spot)] <- "no"
+ggplot(aframe_tmp, aes(PC1, PC2, color = condition, shape = object)) +
+  geom_point() +
+  ggtitle("PCA - SPK") +
+  theme_classic()
+
+# Procession Street ####
+tmp <- meta[meta$object == "Procession Street, left side",]
+ok_samples <- tmp$sample.id
+counts_tmp <- counts[, ok_samples]
+zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
+counts_tmp <- counts_tmp[which(zeros > 1),]
+
+counts_tmp <- counts_tmp[, colSums(counts_tmp) > 100]
+
+normalized <- t(t(counts_tmp)/colSums(counts_tmp))
+normalized <- sqrt(normalized)
+
+mds <- cmdscale(dist(t(normalized)), k = 2)
+pca <- prcomp(t(normalized), scale. = T)
+aframe_tmp <- data.frame(meta[match(colnames(counts_tmp), meta$sample.id), ], total = colSums(counts_tmp), mds, pca$x)
+
+aframe_tmp$height <- c("low", "low", "middle", "middle", "high", "high", "very high", "very high")
+ggplot(aframe_tmp, aes(X1, X2, color = height, shape = object)) +
+  geom_point() +
+  ggtitle("MDS - SPK") +
+  theme_classic()
+
+ggplot(aframe_tmp, aes(X1, X2, color = avg_pred, shape = object)) +
+  geom_point() +
+  ggtitle("MDS - SPK") +
+  theme_classic()
+
+
+# Procession Street ####
+tmp <- meta[meta$object == "Ishtar Gate, right side",]
+ok_samples <- tmp$sample.id
+counts_tmp <- counts[, ok_samples]
+zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
+counts_tmp <- counts_tmp[which(zeros > 1),]
+
+counts_tmp <- counts_tmp[, colSums(counts_tmp) > 100]
+
+normalized <- t(t(counts_tmp)/colSums(counts_tmp))
+normalized <- sqrt(normalized)
+
+mds <- cmdscale(dist(t(normalized)), k = 2)
+pca <- prcomp(t(normalized), scale. = T)
+aframe_tmp <- data.frame(meta[match(colnames(counts_tmp), meta$sample.id), ], total = colSums(counts_tmp), mds, pca$x)
+
+ggplot(aframe_tmp, aes(X1, X2, color = spot, shape = object)) +
+  geom_point() +
+  ggtitle("MDS - SPK") +
+  theme_classic()
+
+ggplot(aframe_tmp, aes(PC1, PC2, color = spot, shape = object)) +
+  geom_point() +
+  ggtitle("PCA - SPK") +
+  theme_classic()
+
+
+# Zeus Sosipolis Tempel, Hellenistic Hall ####
+tmp <- meta[meta$object == "Zeus Sosipolis Tempel, Hellenistic Hall",]
+ok_samples <- tmp$sample.id
+counts_tmp <- counts[, ok_samples]
+zeros <- apply(counts_tmp, 1, function(x) sum(x > 0))
+counts_tmp <- counts_tmp[which(zeros > 1),]
+
+counts_tmp <- counts_tmp[, colSums(counts_tmp) > 100]
+
+normalized <- t(t(counts_tmp)/colSums(counts_tmp))
+normalized <- sqrt(normalized)
+
+mds <- cmdscale(dist(t(normalized)), k = 2)
+pca <- prcomp(t(normalized), scale. = T)
+aframe_tmp <- data.frame(meta[match(colnames(counts_tmp), meta$sample.id), ], total = colSums(counts_tmp), mds, pca$x)
+
+ggplot(aframe_tmp, aes(X1, X2, color = spot, shape = object)) +
+  geom_point() +
+  ggtitle("MDS - SPK") +
+  theme_classic()
 
 # Create some plots ####
-tmp <- log(OTU@.Data + 1)
-tmp <- tmp[which(apply(tmp, 1, var) > 0),]
-pca <- prcomp(t(tmp), scale. = T)
-
-counts <- OTU@.Data
-
-aframe <- data.frame(pca$x, data.frame(info), total_counts = colSums(counts))
-ggplot(aframe, aes(Collection, total_counts, color = Collection)) +
-  geom_boxplot() + geom_point() +
-  theme_bw()
-
-ggplot(aframe, aes(Object, log10(total_counts), color = Object)) +
-  geom_boxplot() + geom_point() +
-  theme_bw()
-
-info <- data.frame(samples)
-info$total <- colSums(counts)
-
-ok <- which(info$Object %in% c("Ischta Gate, right side", "Procession Street, left side") & info$total > 10000)
-ok <- which(info$total > 10000)
-ok <- rownames(info)[ok]
-calc_sums <- function(counts, taxa = "Phylum"){
-  asplit <- split(rownames(counts), TAX@.Data[, taxa])
-  sums <- do.call(rbind, lapply(asplit, function(x) colSums(counts[x, ])))
-  sums
-}
-sums <- calc_sums(counts[, ok], taxa = "Domain")
-
-aframe <- data.frame(t(sums), info[ok,])
-ggplot(aframe, aes(log10(D_1__Cyanobacteria + 1), log10(D_1__Nanoarchaeaeota + 1), color = spot)) +#, D_1__Proteobacteria))
-  geom_point() +
-  theme_bw()
-
 Down_Sample_Matrix <- function (expr_mat)
 {
 min_lib_size <- min(colSums(expr_mat))
@@ -324,39 +518,3 @@ rbinom(1, y, prob)
 down_sampled_mat <- apply(expr_mat, 2, down_sample)
 return(down_sampled_mat)
 }
-
-library(edgeR)
-
-tmp <- log(Down_Sample_Matrix(counts[, ok]) + 1)
-tmp <- tmp[which(apply(tmp, 1, var) > 0), ]
-pca <- prcomp(t(tmp), scale. = T)
-aframe <- data.frame(info[ok,], t(sums))
-
-ok <- which(info$total > 1000)
-ok <- rownames(info)[ok]
-sums <- calc_sums(counts[, ok], taxa = "Domain")
-aframe <- data.frame(info[ok,], t(sums))
-ggplot(aframe, aes(D_0__Archaea, D_0__Bacteria, color = Institution)) +
-  geom_point() +
-  theme_bw()
-
-aframe$touched <- "touched"
-aframe$touched[grep("untouched", aframe$comments)] <- "untouched"
-ggplot(aframe, aes(D_0__Archaea, D_0__Bacteria, color = Object, shape = touched)) +
-  facet_wrap(~ Institution) +
-  geom_point() +
-  theme_bw()
-
-sums <- calc_sums(counts[, ok], taxa = "Species")
-detected <- apply(counts, 1, function(x) sum(x > 0))
-
-prop <- t(t(sums)/colSums(sums))
-pca <- prcomp(t(prop))
-aframe <- data.frame(info[ok,], pca$x)
-ggplot(aframe[aframe$Object %in% c("Procession Street, left side", "Ischta Gate, right side"),],
-       aes(PC1, PC2, color = comments, label = comments)) +
-  geom_point() + geom_label() +
-  theme_bw()
-
-counts[which(!is.na(TAX@.Data[,1])),]
-
