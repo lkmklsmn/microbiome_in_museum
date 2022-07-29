5190c0e1b5cbde0d09bb0cb8ca783bd67748385a
lkmklsmn <lkmklsmn@gmail.com>
2022-07-29 16:49:21 -0500

    Organize code

--- code/create_qiime2_artefact.sh
+++
@@ -1,9 +0,0 @@
-conda activate qiime2
-
-cd /storage/thinc/data/projects/lounsbery/qiime_2
-
-qiime tools import \
-  --type 'SampleData[PairedEndSequencesWithQuality]' \
-  --input-path manifest_seq2.txt \
-  --output-path lounsberty_2.qza \
-  --input-format PairedEndFastqManifestPhred33V2
